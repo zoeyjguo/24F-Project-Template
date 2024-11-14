0037b2ec5549245b6c28a2b9430401b6a3ada321
juliaatan <juliatanbusiness@gmail.com>
2024-11-14 14:28:44 -0500

    add button

--- app/src/Home.py
+++ app/src/Home.py
@@ -34,7 +34,7 @@ SideBarLinks(show_home=True)
 
 # set the title of the page and provide a simple prompt. 
 logger.info("Loading the Home page of the app")
-st.title('CS 3200 Sample Semester Project App')
+st.title('CoLink')
 st.write('\n\n')
 st.write('### HI! As which user would you like to log in?')
 
@@ -73,5 +73,11 @@ if st.button('Act as System Administrator',
     st.session_state['first_name'] = 'SysAdmin'
     st.switch_page('pages/20_Admin_Home.py')
 
+if st.button('Some text on the button',
+             type = 'secondary',
+             use_container_width = True):
+        st.session_state['authenticated'] = True
+        st.session_state['role'] = 'Professor'
+        st.switch_page('pages/20_Admin_Home-py')
 
 
